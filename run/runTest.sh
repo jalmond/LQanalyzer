975157592b3815fcbf1ec456a454a21bf47b58ba
John <jalmond@cern.ch>
2013-10-18 22:12:17 +0900

    delete old log files

--- run/runTest.sh
+++ run/runTest.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+B1;2c#!/bin/sh
 
 echo "Runninh DY and DiMuon period A samples" 
 
 root -l -q -b compile.C
 
 function runCode(){    
+    rm log/$2
     nohup root -l -q -b $1 &> log/$2 &
     echo "running script runDataDiMuonPeriodA.C. . Output log to " log/$2 
  
