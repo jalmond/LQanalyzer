021288233dd0fb04b609dcb8d5956addf306fb7d
John <jalmond@cern.ch>
2013-12-04 23:31:56 +0900

    Update checkduplicate command for MC

--- bin/checkduplicate.py
+++ bin/checkduplicate.py
@@ -3,6 +3,8 @@
 import os,sys, getopt
 
 dir = sys.argv[1]
+tocheck= sys.argv[2]
+print "Number of files in directory = "
 count = os.system("ls -1 " + str(dir) + "  | wc -l")
 
 os.system("cd " + str(dir) );
@@ -11,8 +13,11 @@ os.system("ls " + str(dir) + "/*.root > check.txt")
 isfile = os.path.isfile
 join = os.path.join
 number_of_files = sum(1 for item in os.listdir(dir) if isfile(join(dir, item)))
-tocheck = "RootTupleMakerV2_output_DATA_"
+#tocheck = "RootTupleMakerV2_output_DATA_"
 
+print "Checking for duplicated Job ID :" + tocheck + "[jobid]" + "_##.root"
+
+dupl =0
 for i in range(1, count):
     fullname = tocheck + str(i) + "_"
     nrepeat=0
@@ -20,7 +25,12 @@ for i in range(1, count):
     for line in fr:
         if fullname in line:
             nrepeat+=1
+            #print "Job " +i  + ": --> " +  line
             if nrepeat == 2:
                 print line         
                 nrepeat+=1
                 print "FOUND TWICE"
+                dupl+=1
+if dupl == 0:
+    print "No duplicated jobs in directory"
+os.system("rm check.txt")
