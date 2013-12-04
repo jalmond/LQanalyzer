ab61b2f93abc33ee8f1edfa754f060b938b14948
John <jalmond@cern.ch>
2013-12-04 10:48:14 +0900

    Update copy scripts

--- bin/checkduplicate.py
+++ bin/checkduplicate.py
@@ -1,24 +1,26 @@
-import os
+#!/usr/bin/python
 
-dir = "/data1/SNUData/Data/Electron/DoubleElectron/Nov13/periodC/"
-os.system("cd " + dir );
-os.system("ls *.root > check.txt")
+import os,sys, getopt
+
+dir = sys.argv[1]
+count = os.system("ls -1 " + str(dir) + "  | wc -l")
+
+os.system("cd " + str(dir) );
+os.system("ls " + str(dir) + "/*.root > check.txt")
 
 isfile = os.path.isfile
 join = os.path.join
 number_of_files = sum(1 for item in os.listdir(dir) if isfile(join(dir, item)))
 tocheck = "RootTupleMakerV2_output_DATA_"
 
-for i in range(1, 2181):
+for i in range(1, count):
     fullname = tocheck + str(i) + "_"
     nrepeat=0
     fr = open('check.txt','r')
     for line in fr:
         if fullname in line:
             nrepeat+=1
-#            print str(i) + ": Found "  + line
-        if nrepeat == 2:
-            print line         
-            nrepeat+=1
-            print "FOUND TWICE"
-            
+            if nrepeat == 2:
+                print line         
+                nrepeat+=1
+                print "FOUND TWICE"
--- bin/makeTransferFile_snu.py
+++ bin/makeTransferFile_snu.py
@@ -1,100 +1,84 @@
-import os
-
-#### to change
-sample="LQ_Nov13DiElPeriodB"
-
-#### output location
-snumachine = "cms1"
-endpath="/data/HN_data/Data/Data_doubleElectron/Nov13/periodB/"
-
-#### not to change
-machine="uosaf0007.sscc.uos.ac.kr"
-path= "/cms/store/user/jalmond/"
-dir=sample + "/"
-toremove = path+dir
-print 'length to remove = ' + str(len(toremove))
+def makeTransferFile_snu(snumachine, sample, endpath):
+	import os
+	
+        #### not to change
+	machine="uosaf0007.sscc.uos.ac.kr"
+	path= "/cms/store/user/jalmond/"
+	dir=sample + "/"
+	toremove = path+dir
+	print 'length to remove = ' + str(len(toremove))
     
-###### DO COPY
-os.system("rm *.txt")
+        ###### DO COPY
+	os.system("rm *.txt")
 
-os.system("xrd " + machine + " ls " + path + dir + " > " + sample +".txt")
-os.system("sed -i '/^$/d' " + sample +".txt")
+	if not (os.path.exists(endpath)):
+		os.system("mkdir " + endpath)
+	if not (os.path.exists(endpath)):
+		os.exit();
+			
+	os.system("xrd " + machine + " ls " + path + dir + " > " + sample +".txt")
+	os.system("sed -i '/^$/d' " + sample +".txt")
+	
+	os.system("sed -r 's/^.{43}//' " + sample + ".txt > copy.txt")
+	os.system("cut -d/ -f 7 copy.txt > fullsamplelist.txt")
+	
+	
+        ## output the number of files from grid job
+	
+        ## check if rootfiles already exist in output location
+	print "ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > remove_already_copiedfiles.txt"
+
+	os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > remove_already_copiedfiles.txt")
+	os.system("sed -i '/^$/d' remove_already_copiedfiles.txt")
+	
+	fr = open('remove_already_copiedfiles.txt' ,'r')
+	for line in fr:
+		print 'file ' + line.strip() + ' already copied, removing from list to be copied'  
+		# remove filename from copy.txt
+		os.system("sed -i '/" + line.strip()+"/d' copy.txt")
+		
+		print "removed all files previously copied"
+		
+	f = open('copy.txt' ,'r')
+	for line in f:
+		# copy to local dir
+		print 'copying '+ line.strip()
+		copy = "xrdcopy root://" + machine+"/"
+		fullpath = line.strip()
+		os.system(copy+fullpath + " " + endpath + "/")  
+
+		filename=line.strip()[len(toremove):]
+		print "copied file removed from copy.txt"
+		# remove filename from copy.txt
+		os.system("sed -i '/" + filename+"/d' copy.txt")
+		
+	print "Finished copying to " + snumachine + ".snu.ac.kr:/" + endpath + ". List of files copied are: "   
+	os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./'")
+	os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > files_at_snuend.txt")
+
+	
+	os.system("grep -Fxv -f  files_at_snuend.txt fullsamplelist.txt > missingfiles.txt")
+	os.system("sed -i '/^$/d' missingfiles.txt")
+	
+	fm = open('missingfiles.txt' ,'r')
+	for line in fm:
+		# copy to local dir
+		print 'File not copied originally is: '+ line.strip()  +". Retrying copy" 
+		copy = "xrdcopy root://" + machine+"/"
+		fullpath = line.strip()
+		os.system(copy+fullpath + " " + endpath + "/")
     
-os.system("sed -r 's/^.{43}//' " + sample + ".txt > copy.txt")
-os.system("sed -r 's/^.{" + toremove + "}//' copy.txt > fullsamplelist.txt")
-
-## output the number of files from grid job
-
-## check if rootfiles already exist in output location
-print "ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > remove_already_copiedfiles.txt"
-
-os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > remove_already_copiedfiles.txt")
-os.system("sed -i '/^$/d' remove_already_copiedfiles.txt")
-
-fr = open('remove_already_copiedfiles.txt' ,'r')
-for line in fr:
-    print 'file ' + line.strip() + ' already copied, removing from list to be copied'  
-    # remove filename from copy.txt
-    os.system("sed -i '/" + line.strip()+"/d' copy.txt")
-
-print "removed all files previously copied"
-    
-f = open('copy.txt' ,'r')
-for line in f:
-    # copy to local dir
-    print 'copying '+ line.strip()
-    copy = "xrdcopy root://" + machine+"/"
-    fullpath = line.strip()
-    os.system(copy+fullpath + " ./")  
-    
-    # copy to snu
-    filename=line.strip()[len(toremove):]
-    print "copying " + filename +  " over to cms1 machine"
-    os.system("scp "+ filename + " jalmond@" + snumachine +".snu.ac.kr:" + endpath)#
-
-    # remove file from local dir
-    os.system("rm " + filename)
-
-    print "copied file removed from copy.txt"
-    # remove filename from copy.txt
-    os.system("sed -i '/" + filename+"/d' copy.txt")
-
-
-
-print "Finished copying to " + snumachine + ".snu.ac.kr:/" + endpath + ". List of files copied are: "   
-os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./'")
-os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > files_at_snuend.txt")
-
-os.system("grep -Fxv -f  files_at_snuend.txt fullsamplelist.txt > missingfiles.txt")
-os.system("sed -i '/^$/d' missingfiles.txt")
-
-fm = open('missingfiles.txt' ,'r')
-for line in fm:
-    # copy to local dir
-    print 'File not copied originally is: '+ line.strip()  +". Retrying copy" 
-    copy = "xrdcopy root://" + machine+"/"
-    fullpath = line.strip()
-    os.system(copy+fullpath + " ./")
-    
-    # copy to snu
-    filename=line.strip()[len(toremove):]
-    print "copying " + filename +  " over to cms1 machine"
-    os.system("scp "+ filename + " jalmond@" + snumachine +".snu.ac.kr:" + endpath)
-    
-    # remove file from local dir
-    os.system("rm " + filename)
-    
-    # remove filename from copy.txt
-    os.system("sed -i '/" + filename+"/d' missingfiles.txt")
+		filename=line.strip()[len(toremove):]
+		# remove filename from copy.txt
+		os.system("sed -i '/" + filename+"/d' missingfiles.txt")
                                                     
-print "Doing final check. Files failed to copy ="
-os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > files_at_snuend_v2.txt")
+	print "Doing final check. Files failed to copy ="
+	os.system("ssh jalmond@" + snumachine + ".snu.ac.kr 'cd "+ endpath  +"; ls  ./' > files_at_snuend_v2.txt")
 
-os.system("grep -Fxv -f  files_at_snuend_v2.txt fullsamplelist.txt")
+	os.system("grep -Fxv -f  files_at_snuend_v2.txt fullsamplelist.txt")
 
-# basic check files where copied fully.
 
-os.system("ssh jalmond@cms1.snu.ac.kr cd '/data/HN_data/Data/Data_doubleElectron/Nov13/periodC/; ls -s'")
+
 
 
 
