3e14c3b95bc095457743563fa5e762817ccf36ad
John <jalmond@cern.ch>
2013-12-06 19:03:48 +0900

    Update plotting code

--- LQRun/Macros/PlotZ.C
+++ LQRun/Macros/PlotZ.C
@@ -4,18 +4,30 @@
   gStyle->SetPalette(1);
 
 
-  string maindir = getenv("MAINDIR");
+  string maindir = getenv("LQANALYZER_DIR");
 
-  TFile * file_data = new TFile ((maindir + "/output/DoubleMu_periodA_1.root").c_str());
-  TFile * file_DY_highmass = new TFile ((maindir + "/output/DYhighMass_1.root").c_str());
-  TFile * file_DY_lowmass = new TFile ((maindir + "/output/DYLowMass_1.root").c_str());
-  TH1* h_data = (TH1*)file_data->Get("h_zpeak");
-  TH1* h_dy_lowmass = (TH1*)file_DY_lowmass->Get("h_zpeak");
-  TH1* h_dy_highmass = (TH1*)file_DY_highmass->Get("h_zpeak");
+  TFile * file_data1 = new TFile ((maindir + "/data//output/Analyzer_muon_periodA.root").c_str());
+  TFile * file_data2 = new TFile ((maindir + "/data//output/Analyzer_muon_periodB.root").c_str());
+  TFile * file_data3 = new TFile ((maindir + "/data//output/Analyzer_muon_periodC.root").c_str());
+  TFile * file_data4 = new TFile ((maindir + "/data//output/Analyzer_muon_periodD.root").c_str());
+  TFile * file_DY_highmass = new TFile ((maindir + "/data/output/Analyzer_DY50plus.root").c_str());
+  TFile * file_DY_lowmass = new TFile ((maindir + "/data/output/Analyzer_DY10to50.root").c_str());
+  TH1* h_data = (TH1*)file_data1->Get("h_zpeak_mumu");
+  TH1* h_data2 = (TH1*)file_data2->Get("h_zpeak_mumu");
+  TH1* h_data3 = (TH1*)file_data3->Get("h_zpeak_mumu");
+  TH1* h_data4 = (TH1*)file_data4->Get("h_zpeak_mumu");
+
+  TH1* h_dy_lowmass = (TH1*)file_DY_lowmass->Get("h_zpeak_mumu");
+  TH1* h_dy_highmass = (TH1*)file_DY_highmass->Get("h_zpeak_mumu");
 
   TCanvas * c = new TCanvas("","",800,600);
   c->cd();
   c->SetLogy();
+
+  h_data->Add(h_data2);
+  h_data->Add(h_data3);
+  h_data->Add(h_data4);
+
   h_data->Rebin(5);
   h_data->GetXaxis()->SetRangeUser(15.,200.);
   h_data->GetYaxis()->SetRangeUser(1.,5000000.);
