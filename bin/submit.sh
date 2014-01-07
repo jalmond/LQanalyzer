146b81ea056de9dcdd86bae2fff610f0f392874c
John <jalmond@cern.ch>
2014-01-07 19:11:57 +0900

    add submit file

---
+++ bin/submit.sh
@@ -0,0 +1,35 @@
+
+
+############################################################                                                                                                                    
+################# Do not change anything after this line                                                                                                                        
+############################################################                                                                                                                    
+
+if [ -z ${LQANALYZER_DIR} ]
+    then
+    setupLQANALYZER
+fi
+
+#### FULL LIST OF OPTIONS                                                                                                                                                       
+stream=$(makeParseVariable 's' ${stream})
+njobs=$(makeParseVariable 'j' ${njobs})
+cycle=$(makeParseVariable 'c' ${cycle})
+data_lumi=$(makeParseVariable 'd' ${data_lumi})
+outputdir=$(makeParseVariable 'O' ${outputdir})
+logstep=$(makeParseVariable 'o' ${logstep})
+skipevent=$(makeParseVariable 'k' ${skipevent})
+loglevel=$(makeParseVariable 'l' ${loglevel})
+nevents=$(makeParseVariable 'n' ${nevents})
+totalev=$(makeParseVariable 'e' ${totalev})
+xsec=$(makeParseVariable 'x' ${xsec})
+targetlumi=$(makeParseVariable 'T' ${targetlumi})
+efflumi=$(makeParseVariable 'E' ${efflumi})
+remove=$(makeParseVariable 'w' ${remove_workspace})
+skinput=$(makeParseVariable 'S' ${skinput})
+runevent=$(makeParseVariable 'R' ${runevent})
+################                                                                                                                                                                
+#submit                                                                                                                                                                         
+for i in ${input_samples[@]}
+  do
+  python ${LQANALYZER_DIR}/python/localsubmit.py -p ${i} ${stream} ${njobs} ${cycle} ${logstep} ${data_lumi} ${outputdir} ${remove} ${loglevel} ${skipevent} ${nevents} ${totalev} ${xsec} ${targetlumi} ${efflumi} ${remove} ${skinput} ${runevent}
+done
+
