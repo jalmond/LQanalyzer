941e75c72434c9a478891fc32187d96e6cadd3c4
jalmond <jalmond@cern.ch>
2014-03-06 11:08:42 +0900

    new script

--- LQRun/treemaker_538.sh
+++ LQRun/treemaker_538.sh
@@ -17,7 +17,6 @@ njobs=30
 data_lumi="AtoD"
 loglevel="INFO"
 logstep=1000
-declare -a input_samples=("DY10to50" "DY50plus" "ttbar" "Wjets" "WZ" "ZZ" "WW" "W-W-" "W+W+" "Wgamma")
-
+declare -a input_samples=("A" "B" "C" "D" "DY10to50" "DY50plus" "ttbar" "Wjets" "WZ" "ZZ" "WW" "W-W-" "W+W+" "Wgamma")
 ### submit this configured job (uses bin/submit.sh)
 source submit.sh
