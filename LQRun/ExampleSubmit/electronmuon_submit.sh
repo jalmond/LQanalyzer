9e29c755c3876d2617cd03093531a2ddf7b5a00a
jalmond <jalmond@cern.ch>
2014-04-01 00:18:22 +0900

    add electronmuon submit

---
+++ LQRun/ExampleSubmit/electronmuon_submit.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+###### SET WHAT JOBS TO RUN
+runMC=true
+runElectronMuon=true
+
+if [[ $runMC  == "true" ]]; 
+then
+    source functions.sh
+    cycle="ExampleAnalyzerElectronMuon"
+    skinput="True"
+    useskim="DiLep"
+    outputdir="/home/jalmond/LQanalyzer/data/output/ElectronMuon/"
+    #### JOB CONFIGURATION
+    njobs=30
+    data_lumi="AtoD"
+    loglevel="INFO"
+    logstep=1000
+    
+    declare -a input_samples=("DY10to50" "DY50plus" "ttbar" "Wjets" "WZ" "ZZ" "WW" )
+    source submit.sh $1
+fi
+    
+
+################ DOUBLEMUON DATA
+if [[ $runElectronMuon  == "true" ]];
+then
+    source functions.sh
+    cycle="ExampleAnalyzerElectronMuon"
+    skinput="True"
+    stream="emu"
+    useskim="DiLep"
+    outputdir="/home/jalmond/LQanalyzer/data/output/ElectronMuon/"
+    #### JOB CONFIGURATION
+    njobs=30
+    data_lumi="AtoD"
+    loglevel="INFO"
+    logstep=1000
+
+    declare -a input_samples=("A" "B" "C" "D")
+    source submit.sh $1
+fi     
+
+
+
+
+
+echo ""
+echo "End of example_submit.sh script."
