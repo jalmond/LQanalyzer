654b98b2171ad5da4cd93015b4199608d4a7b987
jalmond <jalmond@cern.ch>
2015-08-12 01:36:41 +0900

    update location of new samples

--- LQRun/SKTreeMakerSubmit/treemaker_5314_dilepskim.sh
+++ LQRun/SKTreeMakerSubmit/treemaker_5314_dilepskim.sh
@@ -58,7 +58,7 @@ then
     declare -a input_samples=("stbar_sch" "stbar_tch" "stbar_tW" "st_sch" "st_tch" "st_tW"  "Wbb" "Zbb""QCD_20_30_EM" "QCD_20_30_BCtoE" "QCD_30_80_EM" "QCD_30_80_BCtoE" "QCD_80_170_EM" "QCD_80_170_BCtoE" "QCD_170_250_EM" "QCD_170_250_BCtoE" "QCD_250_350_EM" "QCD_250_350_BCtoE" "WZ_py" "ZZ_py" "WW_py")
 
     declare -a input_samples=("SSWmWm" "WZ_py" "ZZ_py" "WW_py" "SSWpWp" "WW_dp" "ttW" "ttZ" "TTWW" "TTG" "ZZZ" "WZZ" "WWZ" "HtoTauTau" "HtoWW"   "WWW" "WWG" "HtoZZ")
-    declare -a input_samples=("DY10to50" "DY50plus") 
+    declare -a input_samples=("DY10to50_15" "DY50plus_15") 
 
     source submit.sh
 fi
--- LQRun/SKTreeMakerSubmit/treemaker_5314_lepskim.sh
+++ LQRun/SKTreeMakerSubmit/treemaker_5314_lepskim.sh
@@ -76,15 +76,9 @@ then
     loglevel="INFO"
     logstep=1000
     
-    #    declare -a input_samples=("DY10to50" "ttbar" "Wjets" "WZ_py" "ZZ_py" "WW_py" "QCD_30-40_EM2" "QCD_40_EM2" "Wgamma" "SSWmWm" "SSWpWp" "WW_dp" "ttW" "ttZ" "stbar_sch" "stbar_tch" "stbar_tW" "st_sch" "st_tch" "st_tW"  "Wbb" "Zbb" "DYee" "DYmm" "DYtt" "ggHtoZZ"  "Zgamma" "WgammaE" "WW_mg" "WZtollqq_mg" "WZtoqqln_mg" "WZtollln_mg" "ZZtollnn_mg" "ZZtollqq_mg" "ZZtollll_mg" "ZZtoeemm" "ZZtoeett" "ZZtommtt" "ZZtommmm" "ZZtoeeee" "ZZtotttt" "QCD_20_30_EM" "QCD_20_30_BCtoE" "QCD_30_80_EM" "QCD_30_80_BCtoE" "QCD_80_170_EM" "QCD_80_170_BCtoE" "QCD_170_250_EM" "QCD_170_250_BCtoE" "QCD_250_350_EM" "QCD_250_350_BCtoE" "TTWW" "TTG" "ZZZ" "WZZ" "WWZ" "HtoTauTau" "HtoWW" "WgammaMu" "WgammaTau" "WWW" "WWG" "HtoZZ" "HtoWW" "HtoTauTau" "QCD_350_EM" "QCD_350_BCtoE" "WW_dp" "DY50plus")
 
-    #declare -a input_samples=("stbar_sch" "stbar_tch" "stbar_tW" "st_sch" "st_tch" "st_tW"  "Wbb" "Zbb" "QCD_20_30_EM" "QCD_20_30_BCtoE" "QCD_30_80_EM" "QCD_30_80_BCtoE" "QCD_80_170_EM" "QCD_80_170_BCtoE" "QCD_170_250_EM" "QCD_170_250_BCtoE" "QCD_250_350_EM" "QCD_250_350_BCtoE" "WZ_py" "ZZ_py" "WW_py")
-    #declare -a input_samples=("WgammaTau" "WWW" "WWG" "HtoZZ" "HtoWW" "HtoTauTau" "QCD_350_EM" "QCD_350_BCtoE" "WW_dp" "Wgamma" "SSWmWm" "SSWpWp" "ttW" "ttZ" "Zgamma" "WgammaE" "WW_mg" "WZtollqq_mg"  "WZtoqqln_mg" "WZtollln_mg" "ZZtollnn_mg" "ZZtollqq_mg" "ZZtollll_mg")
-    
-    
 
-    #declare -a input_samples=("SSWmWm" "WZ_py" "ZZ_py" "WW_py" "SSWpWp" "WW_dp" "ttW" "ttZ" "TTWW" "TTG" "ZZZ" "WZZ" "WWZ" "HtoTauTau" "HtoWW"   "WWW" "WWG" "HtoZZ")
-    declare -a input_samples=("DY10to50" "DY50plus")
+    declare -a input_samples=("DY10to50_15" "DY50plus_15")
     source submit.sh
 fi    
 ################ DOUBLEELECTRON DATA
