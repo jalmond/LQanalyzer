39d5fb3404b8eb0d0b714eddec932325b739c817
jalmond <jalmond@cern.ch>
2015-10-13 03:59:11 +0900

    update: submit scripts

--- LQRun/ExampleSubmit/dielectron_submit.sh
+++ LQRun/ExampleSubmit/dielectron_submit.sh
@@ -1,55 +1,132 @@
 #!/bin/sh
 
 ###### SET WHAT JOBS TO RUN
-runMC=false
+runMC=true
 runDoubleElectron=true
+
+## RUN PARAMETERS
+job_cycle="ExampleAnalyzerDiElectron"
+job_useskim="DiLep" ### "Lepton" for single lepton skim   "DiLep" for dilepton skim
+job_stream="egamma" ### "egamma" for DoubleEG dataset "muon" for DoubleMuon dataset
+job_skinput="True"
+
+job_data_lumi="C"  ###  "C" = period C only   "ALL"  = period C+D
+job_logstep=1000
+job_loglevel="INFO"
+job_njobs=15
+
+############################################################
+
+outputdir_cat=$LQANALYZER_DIR"/data/output/CAT/"
+outputdir_electron=$LQANALYZER_DIR"/data/output/CAT/Electron/"
+if [[ ! -d "${outputdir_cat}" ]]; then
+    mkdir ${outputdir_cat}
+    echo "Making " + ${outputdir_cat}
+fi
+
+if [[ ! -d "${outputdir_electron}" ]]; then
+    mkdir ${outputdir_electron}
+    echo "Making " + ${outputdir_electron}
+fi
+
+dir_tag="periodC/"
+if [[ $job_data_lumi  == "ALL" ]];
+    then
+    dir_tag="periodCtoD/"
+fi
+
+outputdir_mc=$LQANALYZER_DIR"/data/output/CAT/Electron/"$dir_tag
+outputdir_data=$LQANALYZER_DIR"/data/output/CAT/Electron/"${dir_tag}$"/Data/"
+echo $outputdir_data
+echo ${outputdir_data}
+output_datafile=$LQANALYZER_DIR"/data/output/CAT/Electron/"${dir_tag}$"/"$job_cycle"_data_catv4_7_2.root"
+echo $output_datafile
+
+
+
+
+if [[ ! -d "${outputdir_mc}" ]]; then
+    mkdir ${outputdir_mc}
+    echo "Making " + ${outputdir_mc}
+fi
+
+if [[ ! -d "${outputdir_data}" ]]; then
+    mkdir ${outputdir_data}
+    echo "Making " + ${outputdir_data}
+fi
+
+########################################################################
+
 
 if [[ $runMC  == "true" ]]; 
 then
     source functions.sh
-    cycle="ExampleAnalyzerDiElectron"
+    
+
+    cycle=$job_cycle
+    data_lumi=$job_data_lumi
+    stream=$job_stream
+    useskim=$job_useskim
     skinput="True"
-    outputdir=$LQANALYZER_DIR"/data/output/CAT/Electron/periodCtoD/"
-    #### JOB CONFIGURATION
-    njobs=30
-    data_lumi="ALL"
-    loglevel="INFO"
-    logstep=1000
-    useskim="DiLep"
+    njobs=$job_njobs
+    outputdir=${outputdir_mc}
+
     
     declare -a input_samples=("DY10to50" "DY50plus" "TTJets_MG5" "WZ" "ZZ" "WW" "singletop_tbar" "singletop_t" "singletop_tbarW" "singletop_tW" "ttWJetsToLNu" "ttWJetsToQQ" "ttZToLLNuNu" "ttZToQQ")
-    
+     declare -a input_samples=("singletop_t")
 
     source submit.sh $1
 fi
-    
+
 
 ################ DOUBLEELECTRON DATA
-### submit this configured job (uses bin/submit.sh)
 if [[ $runDoubleElectron  == "true" ]];
 then
     source functions.sh
-    cycle="ExampleAnalyzerDiElectron"
+    
+    cycle=$job_cycle
+    data_lumi=$job_data_lumi
+    stream=$job_stream
+    useskim=$job_useskim
     skinput="True"
-    stream="egamma"
-    outputdir=$LQANALYZER_DIR"/data/output/CAT/Electron/periodCtoD/Data/"
-    #### JOB CONFIGURATION
-    njobs=30
-    data_lumi="ALL"
-    useskim="DiLep"
+    njobs=$job_njobs
+    
+    skinput="True"
+    
+    outputdir=${outputdir_data}
+    
+    if [[ $job_data_lumi  == "ALL" ]];
+        then
+        declare -a input_samples=("C" "D")
+    fi
 
-    loglevel="INFO"
-    logstep=1000
+    if [[ $job_data_lumi  == "C" ]];
+        then
+        declare -a input_samples=("C")
+    fi
 
-    declare -a input_samples=("C" "D")
     source submit.sh $1
-
-    source hadd.sh ${LQANALYZER_DIR}/data/output/CAT/Electron/periodCtoD/Data/ExampleAnalyzerDiElectron_data_catv4_7_2.root ExampleAnalyzerDiElectron*
-    mv  ${LQANALYZER_DIR}/data/output/CAT/Electron/periodCtoD/Data/ExampleAnalyzerDiElectron_data_catv4_7_2.root  ${LQANALYZER_DIR}/data/output/CAT/Electron/periodCtoD/
-    #mv  ${LQANALYZER_DIR}/data/output/CAT/Electron/periodC/Data/*periodC* ${LQANALYZER_DIR}/data/output/CAT/Electron/periodC/ExampleAnalyzerDiElectron_data_catv4_7_2.root
-
-fi     
-
+    
+    
+    
+    if [[ $job_data_lumi  == "C" ]];
+        then
+        echo "Ran only period C"
+	
+        mv  ${outputdir_data}/*.root ${output_datafile}
+	
+	
+    fi
+    if [[  $job_data_lumi  != "C" ]];
+        then
+        echo "source hadd.sh " ${outputdir_data} " " ${job_cycle} "_data_catv4_7_2.root "${job_cycle}"*"
+	
+        source hadd.sh ${outputdir_data} ${job_cycle}_data_catv4_7_2.root ${job_cycle}*
+        echo ${outputdir_data}"/"${job_cycle}"_data_catv4_7_2.root "
+        mv  ${outputdir_data}/${job_cycle}_data_catv4_7_2.root  ${outputdir_mc}
+    fi
+    
+fi
 
 
 
--- LQRun/ExampleSubmit/dimuon_submit.sh
+++ LQRun/ExampleSubmit/dimuon_submit.sh
@@ -4,31 +4,78 @@
 runMC=true
 runDoubleMuon=true
 
+
 ## RUN PARAMETERS
-#logstep=1000
-#loglevel="INFO"
-#cycle="ExampleAnalyzerDiMuon"
-data_lumi="ALL"
-#    useskim="DiLep"
-#    stream="muon"
+job_cycle="ExampleAnalyzerDiMuon"
+job_data_lumi="ALL"   ###  "C" = period C only   "ALL"  = period C+D
+job_stream="muon" ### "egamma" for DoubleEG dataset "muon" for DoubleMuon dataset 
+job_skinput="True" 
+
+job_useskim="DiLep" ### "Lepton" for single lepton skim   "DiLep" for dilepton skim
+job_logstep=1000
+job_loglevel="INFO"
+job_njobs=15
+
+
+########################################################################
+
+
+outputdir_cat=$LQANALYZER_DIR"/data/output/CAT/"
+outputdir_muon=$LQANALYZER_DIR"/data/output/CAT/Muon/"
+if [[ ! -d "${outputdir_cat}" ]]; then
+    mkdir ${outputdir_cat}
+    echo "Making " + ${outputdir_cat}
+fi
+
+if [[ ! -d "${outputdir_muon}" ]]; then
+    mkdir ${outputdir_muon}
+    echo "Making " + ${outputdir_muon}
+fi
+
+dir_tag="periodC/"
+if [[ $job_data_lumi  == "ALL" ]];
+    then 
+    dir_tag="periodCtoD/"
+fi
+
+outputdir_mc=$LQANALYZER_DIR"/data/output/CAT/Muon/"$dir_tag
+outputdir_data=$LQANALYZER_DIR"/data/output/CAT/Muon/"${dir_tag}$"/Data/"
+echo $outputdir_data
+echo ${outputdir_data}
+output_datafile=$LQANALYZER_DIR"/data/output/CAT/Muon/"${dir_tag}$"/"$job_cycle"_data_catv4_7_2.root"
+echo $output_datafile
+
+#echo "source hadd.sh " ${outputdir_data} " " ${job_cycle} "_data_catv4_7_2.root "${job_cycle}"*" 
+
+if [[ ! -d "${outputdir_mc}" ]]; then
+    mkdir ${outputdir_mc}
+    echo "Making " + ${outputdir_mc}
+fi
+
+if [[ ! -d "${outputdir_data}" ]]; then
+    mkdir ${outputdir_data}
+    echo "Making " + ${outputdir_data}
+fi
+
+########################################################################
+
 
 if [[ $runMC  == "true" ]]; 
 then
     source functions.sh
     
-    cycle="ExampleAnalyzerDiMuon"
-    data_lumi="C"
-    stream="muon"
+    cycle=$job_cycle
+    data_lumi=$job_data_lumi
+    stream=$job_stream
+    useskim=$job_useskim
+    skinput="True"
+    njobs=$job_njobs
+    outputdir=${outputdir_mc}
 
-    useskim="DiLep"
-#    skinput="True"
-    outputdir=$LQANALYZER_DIR"/data/output/CAT/Muon_C/"
-    njobs=30
-    declare -a input_samples=("DY10to50" "DY50plus" "TTJets_MG5" "WZ" "ZZ" "WW" )
-#"QCD_mu20to30" "QCD_mu30to50" "QCD_mu50to80" "QCD_mu80to120" "QCD_mu120to170" "QCD_mu170to300" "QCD_mu300to470" "QCD_mu470to600" "QCD_mu600to800" "QCD_mu800to1000" "QCD_mu1000toINF"  "ttWJetsToLNu" "ttWJetsToQQ" "ttZToLL" "ttZToQQ")
-
+    declare -a input_samples=("DY10to50" "DY50plus" "TTJets_MG5" "WZ" "ZZ" "WW" "singletop_tbar" "singletop_t" "singletop_tbarW" "singletop_tW" "ttWJetsToLNu" "ttWJetsToQQ" "ttZToLLNuNu" "ttZToQQ")
+    declare -a input_samples=("singletop_tW")
+    
     source submit.sh $1
-
 fi
 
 
@@ -37,21 +84,47 @@ if [[ $runDoubleMuon  == "true" ]];
 then
     source functions.sh
 
-    cycle="ExampleAnalyzerDiMuon"
- #   skinput="True"
-    data_lumi="ALL"
-    useskim="DiLep"
-    stream="muon"
-    njobs=30
-    outputdir=$LQANALYZER_DIR"/data/output/CAT/Muon_C/Data/"
-    #### JOB CONFIGURATION
+    cycle=$job_cycle
+    data_lumi=$job_data_lumi
+    stream=$job_stream
+    useskim=$job_useskim
+    skinput="True"
+    njobs=$job_njobs
 
-    declare -a input_samples=("C")
-#    declare -a input_samples=("D")
+    skinput="True"
 
+    outputdir=${outputdir_data}
+    
+    if [[ $job_data_lumi  == "ALL" ]];
+	then
+	declare -a input_samples=("C" "D")
+    fi
+    
+    if [[ $job_data_lumi  == "C" ]];
+	then
+	declare -a input_samples=("C")
+    fi
+    
     source submit.sh $1
-    source hadd.sh ${LQANALYZER_DIR}/data/output/CAT/Muon_C/Data/  ExampleAnalyzerDiMuon_data_catv4_7_2.root ExampleAnalyzerDiMuon*
-    mv  ${LQANALYZER_DIR}/data/output/CAT/Muon_C/Data/ExampleAnalyzerDiMuon_data_catv4_7_2.root  ${LQANALYZER_DIR}/data/output/CAT/Muon_C/
+    
+
+    
+    if [[ $job_data_lumi  == "C" ]]; 
+	then
+	echo "Ran only period C"
+
+	mv  ${outputdir_data}/*.root ${output_datafile}
+
+
+    fi
+    if [[  $job_data_lumi  != "C" ]];
+	then
+	echo "source hadd.sh " ${outputdir_data} " " ${job_cycle} "_data_catv4_7_2.root "${job_cycle}"*"
+
+	source hadd.sh ${outputdir_data} ${job_cycle}_data_catv4_7_2.root ${job_cycle}*
+	echo ${outputdir_data}"/"${job_cycle}"_data_catv4_7_2.root "
+	mv  ${outputdir_data}/${job_cycle}_data_catv4_7_2.root  ${outputdir_mc}
+    fi
 fi
 
 
