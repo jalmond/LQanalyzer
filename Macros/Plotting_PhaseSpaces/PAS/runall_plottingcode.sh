72eadea8f2bc702440801a6bedec06934405bc3f
jalmond <jalmond@cern.ch>
2015-10-13 05:02:41 +0900

    update plotting code

--- Macros/Plotting_PhaseSpaces/PAS/runall_plottingcode.sh
+++
@@ -1,9 +0,0 @@
-cd HvyNPlottingMacroDataVsMC_EMUHighMassRegion/      ;   ./MakeDataMCCompPlots Config/hnplots.txt ;
-cd ../HvyNPlottingMacroDataVsMC_LowMassRegion_noteversion/; ./MakeDataMCCompPlots Config/hnplots.txt ;
-cd ../HvyNPlottingMacroDataVsMC_EMUHighMassRegion_png/; ./MakeDataMCCompPlots Config/hnplots.txt ;         
-cd ../HvyNPlottingMacroDataVsMC_LowMassRegion_noteversion_png/; ./MakeDataMCCompPlots Config/hnplots.txt ;
-cd ../HvyNPlottingMacroDataVsMC_EMULowMassRegion/; ./MakeDataMCCompPlots Config/hnplots.txt ;              
-cd ../HvyNPlottingMacroDataVsMC_MidMass_noteversion/; ./MakeDataMCCompPlots Config/hnplots.txt ;
-cd ../HvyNPlottingMacroDataVsMC_EMULowMassRegion_png/; ./MakeDataMCCompPlots Config/hnplots.txt ;          
-cd ../HvyNPlottingMacroDataVsMC_MidMass_noteversion_png/; ./MakeDataMCCompPlots Config/hnplots.txt ;
-cd ../;
