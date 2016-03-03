92d6fd3a8b001e252ebe9f821ff287a2a8472985
jalmond <jalmond@cern.ch>
2016-03-04 03:28:58 +0900

    Add change log

---
+++ bin/changelog.sh
@@ -0,0 +1,6 @@
+if [[ $CATVERSION == "v7-6-3" ]];
+    then
+    
+    echo "Since this tag has many changed please check out https://twiki.cern.ch/twiki/pub/CMS/HeavyMajoranaNeutrino13TeV/NewLQAnalyzerv763.pdf" >> email.txt
+
+fi
--- bin/mail_tag.sh
+++ bin/mail_tag.sh
@@ -5,5 +5,10 @@ echo " " >> email.txt
 echo "git checkout -b  CatAnalyzer_13TeV<tagname> <tagname>" >> email.txt
 echo "i.e, git checkout -b CatAnalyzer_13TeV_"$CATVERSION " "$CATVERSION  >> email.txt
 echo " " >> email.txt
+echo " " >> email.txt
+echo "Changed with respect to last tag are:" >> email.txt
+source changelog.sh
+echo " " >> email.txt
+
 echo "Thanks," >> email.txt
 echo "John" >> email.txt
