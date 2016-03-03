ab2f8aead07f48bc1a09a7f8c164c61da6eb6c9d
jalmond <jalmond@cern.ch>
2016-03-04 02:52:40 +0900

    fix script

--- scripts/git_newbranch.sh
+++ scripts/git_newbranch.sh
@@ -1,12 +1,13 @@
+todelete=$1
+
+if [[ $todelete == "-d" ]];
+    then
+    git tag -d $CATVERSION
+    git push origin :refs/tags/$CATVERSION
+fi
+
 git commit -a
 git push
 git tag $CATVERSION
 git push --tags
 
-todelete=$1
-echo $todelete
-if [[ $1 == "-d" ]] then
-    
-    git tag -d $CATVERSION 
-    git push origin :refs/tags/$CATVERSION
-fi
