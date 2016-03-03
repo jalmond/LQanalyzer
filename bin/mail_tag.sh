d5486eb151782ab9f38e5918ab0cf3a483eec664
jalmond <jalmond@cern.ch>
2016-03-04 03:21:15 +0900

    Fix email

--- bin/mail_tag.sh
+++ bin/mail_tag.sh
@@ -1,6 +1,9 @@
 echo "Dear User " >> email.txt
 echo " " >> email.txt
 echo "A new tag named "$CATVERSION" is now available in LQanalyzer." >> email.txt
-
+echo " " >> email.txt
+echo "git checkout -b  CatAnalyzer_13TeV<tagname> <tagname>" >> email.txt
+echo "i.e, git checkout -b CatAnalyzer_13TeV_"$CATVERSION " "$CATVERSION  >> email.txt
+echo " " >> email.txt
 echo "Thanks," >> email.txt
 echo "John" >> email.txt
