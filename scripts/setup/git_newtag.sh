be631077e10aed9a39f6699d24430a6d55426015
jalmond <jalmond@cern.ch>
2016-03-08 06:01:16 +0900

    New Tag v7-6-3.3 Fix commit

--- scripts/setup/git_newtag.sh
+++ scripts/setup/git_newtag.sh
@@ -10,17 +10,30 @@ rm $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "export CATVERSION="$CATVERSION >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "### If there is a small bug/new code then new subtag is made"  >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "tag_numerator='"$itag"'"  >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
-echo "if [[ '$1' == "branch" ]];"  >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
+echo "if [[ '-d' == "branch" ]];"  >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "    then" >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "    export CATTAG=" >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "else" >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "    export CATTAG=$CATVERSION"$itag >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 echo "fi" >> $LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 
-todelete=""
 sendemail=false
 
-####### commit with no tag name
+tag_exists=false
+git tag>> gitcheck.txt
+while read line
+  do
+  if [[ $line  == *$tagname* ]];
+      then
+      tag_exists=true
+  fi
+done < gitcheck.txt
+
+echo $tag_exists
+rm gitcheck.txt
+
+
+###### commit with no tag name
 cp $LQANALYZER_DIR/scripts/setup/branch_setup.sh $LQANALYZER_DIR/setup.sh
 git commit -a
 git push
@@ -33,25 +46,18 @@ if [[ $1 == "" ]];
     echo "Making new tag "$tagname
 fi
 
-if [[ $1 == "-d" ]];
+
+
+if [[ $tag_exists == "true" ]];
     then
     git tag -d $tagname
     git push origin :refs/tags/$tagname
     
-    if [[ $2 ==  "-e" ]];
-	then
-	sendemail=true;
-    fi
 fi
 
 if [[ $1 == "-e" ]];
     then
     sendemail=true;
-    if [[ $2 == "-d" ]];
-    then
-	git tag -d $tagname
-	git push origin :refs/tags/$tagname
-    fi
 fi
 
 
