1f4b3ec34452e7aca5c230dcb5c3d262e3da7076
jalmond <jalmond@cern.ch>
2016-03-08 01:10:15 +0900

    New tag v7-6-3.3

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,7 +1,7 @@
 export CATVERSION=v7-6-3
 ### If there is a small bug/new code then new subtag is made
 tag_numerator='.3'
-if [[ '-d' == branch ];
+if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
