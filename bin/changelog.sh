0c574500a3899f25b27fa3e19dc5da3f323bb538
jalmond <jalmond@cern.ch>
2016-03-07 22:40:37 +0900

    Fix in email code

--- bin/changelog.sh
+++ bin/changelog.sh
@@ -1,5 +1,5 @@
 if [[ $CATVERSION ==  "v7-6-3" ]];
-    
+    then
     if [[ $CATTAG == "v7-6-3" ]];
 	then
 	echo "Since this tag has many changed please check out https://twiki.cern.ch/twiki/pub/CMS/SNULQNtuples/NewLQAnalyzerv763.pdf " >> email.txt
