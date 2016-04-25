34077e8f57e36d98060fd02724723954cff0d294
Geumbong Yu <geumbong.yu@gmail.com>
2016-04-25 18:09:13 +0900

    add new files

---
+++ LQAnalysis/include/CHMJ_bestfit.h
@@ -0,0 +1,43 @@
+#ifndef CHMJ_bestfit_h
+#define CHMJ_bestfit_h
+
+#include "AnalyzerCore.h"
+
+#include <iostream>
+#include <TROOT.h>
+#include <TMinuit.h>
+
+class CHMJ_bestfit : public AnalyzerCore{ 
+
+ public:
+	//// constructors                                                                                                                                                             
+	CHMJ_bestfit();
+	~CHMJ_bestfit();
+
+	/// Functions from core
+	virtual void BeginCycle() throw( LQError );
+	virtual void BeginEvent()throw( LQError );
+	virtual void ExecuteEvents()throw( LQError );
+	virtual void EndCycle()throw( LQError );
+	virtual void ClearOutputVectors()throw( LQError );
+
+	void InitialiseAnalysis() throw( LQError );
+	void MakeHistograms();
+	void FillCutFlow(TString cut, float w);
+
+ private:
+
+	//
+	// The output variables 
+	//
+	/// Vectors for output objetcs
+	std::vector<snu::KMuon> out_muons;
+	std::vector<snu::KElectron> out_electrons;
+
+	void mysort(std::vector<snu::KJet> jetColl,int jidx[]);
+	void Sortout(float in1, float in2, float in3, float& out1, float& out2, float& out3);
+
+	
+	ClassDef ( CHMJ_bestfit, 1);    
+};
+#endif
