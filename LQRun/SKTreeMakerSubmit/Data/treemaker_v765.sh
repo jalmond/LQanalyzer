897a46ec8a5ad1c0329b2ffca20bf0f9df208e52
jalmond <jalmond@cern.ch>
2016-06-17 18:10:10 +0900

    Second set of commits for v765

---
+++ LQRun/SKTreeMakerSubmit/Data/treemaker_v765.sh
@@ -0,0 +1,2 @@
+sktree -a SKTreeMaker -S DATA  -n 30 -c v7-6-5 -m "making sktree for v765: first samples"
+sktree -a SKTreeMakerDiLep -S DATADILEP  -n 30 -c v7-6-5  -m "making sktree for v765: first samples"
