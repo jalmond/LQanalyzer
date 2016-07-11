5f08c2a9de6e09f655456a60f616c0dd3239a7a9
jalmond <jalmond@cern.ch>
2016-07-11 20:13:13 +0900

    add enw files

---
+++ LQRun/SKTreeMakerSubmit/Data/treemaker_v766.sh
@@ -0,0 +1,2 @@
+sktree -a SKTreeMaker -S DATA  -n 200 -c v7-6-6 -m "making sktree for v765: first samples"
+sktree -a SKTreeMakerDiLep -S DATADILEP  -n 200 -c v7-6-6  -m "making sktree for v765: first samples"
