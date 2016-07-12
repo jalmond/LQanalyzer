c66ec22655ddf6187de7372cb5c412aaedbdd394
jalmond <jalmond@cern.ch>
2016-07-12 18:35:39 +0900

    Fix dxy in sktree

--- LQRun/SKTreeMakerSubmit/Data/treemaker_v766.sh
+++ LQRun/SKTreeMakerSubmit/Data/treemaker_v766.sh
@@ -1,2 +1,2 @@
-sktree -a SKTreeMaker -S DATA  -n 200 -c v7-6-6 -m "making sktree for v765: first samples"
-sktree -a SKTreeMakerDiLep -S DATADILEP  -n 200 -c v7-6-6  -m "making sktree for v765: first samples"
+sktree -a SKTreeMaker -S DATA  -n 200 -c v7-6-6 -m "Fix dxy for muons"
+sktree -a SKTreeMakerDiLep -S DATADILEP  -n 200 -c v7-6-6  -m "fix dxy for muons"
