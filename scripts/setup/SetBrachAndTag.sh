04bc0f9a4f7c1ab55ba617df3d385d8a9798a079
jalmond <jalmond@cern.ch>
2016-08-16 06:02:35 +0900

    New Tag for 766

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v7-6-6
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.1'
+export tag_numerator='.2'
 if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
-    export CATTAG=v7-6-6.1
+    export CATTAG=v7-6-6.2
 fi
 
