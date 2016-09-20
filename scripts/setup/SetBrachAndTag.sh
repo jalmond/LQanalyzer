3e42b2c9ead8f768a203cc0a582f0550a74fcf18
jalmond <jalmond@cern.ch>
2016-09-20 23:55:09 +0900

    New branch with SFs for leptons IDs

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-1
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.1'
+export tag_numerator='.2'
 if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-1.1
+    export CATTAG=v8-0-1.2
 fi
 
