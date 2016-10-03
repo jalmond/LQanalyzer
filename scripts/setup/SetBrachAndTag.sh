cd2498607b846fc569d145e95e42a25c6f85cf43
jalmond <jalmond@cern.ch>
2016-10-03 20:11:00 +0900

    Fix bug in cluster info

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-1
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.6'
+export tag_numerator='.7'
 if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-1.6
+    export CATTAG=v8-0-1.7
 fi
 
