d70170a3935f903db3594d5cfa22eb9dbdc706f8
jalmond <jalmond@cern.ch>
2016-10-14 02:13:51 +0900

    Several changes: run sktree -tagdiff v7-6-6.7 v7-6-6.8 to see changes

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v7-6-6
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.7'
+export tag_numerator='.8'
 if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
-    export CATTAG=v7-6-6.7
+    export CATTAG=v7-6-6.8
 fi
 
--- scripts/setup/git_newtag.sh
+++ scripts/setup/git_newtag.sh
@@ -4,7 +4,7 @@ if [[ $LQANALYZER_DIR == "" ]];
 fi
 
 ########## Tag index
-itag=".7"
+itag=".8"
 tagname=$CATVERSION$itag
 
 
