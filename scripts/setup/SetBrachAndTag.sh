2e1b50c2af57d8f7c362d7630fe0fe8bea910141
jalmond <jalmond@cern.ch>
2016-12-01 08:34:50 +0900

    Fix in electron ID which effects jet cleaning

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-2
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.7'
+export tag_numerator='.8'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-2.7
+    export CATTAG=v8-0-2.8
 fi
 
