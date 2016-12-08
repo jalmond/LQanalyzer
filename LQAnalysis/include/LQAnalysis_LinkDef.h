331628cd6949ab10dd8f5e05d248116a9cb7af49
jbhyun <jbhyun@cern.ch>
2016-12-09 03:28:25 +0900

    BranchReadyToMerge

---
+++ LQAnalysis/include/LQAnalysis_LinkDef.h
@@ -0,0 +1,20 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+
+#pragma link C++ class AnalyzerCore+;
+#pragma link C++ class ExampleAnalyzer+;
+#pragma link C++ class HNDiElectron+;
+#pragma link C++ class FakeRateCalculator_El+;
+#pragma link C++ class SKTreeMaker+;
+#pragma link C++ class SKTreeMakerDiLep+;
+#pragma link C++ class SKTreeMakerTriLep+;
+#pragma link C++ class SKTreeMakerNoCut+;
+#pragma link C++ class HNEMu+;
+#pragma link C++ class SKTreeValidation+;
+
+#endif // __CINT__
