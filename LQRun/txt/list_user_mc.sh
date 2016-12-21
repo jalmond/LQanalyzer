6008c57d391425d63821f000327f5e4c0dfb4681
Sungbin Oh <sungbin.oh@cern.ch>
2016-12-21 18:27:25 +0900

    pull request

--- LQRun/txt/list_user_mc.sh
+++ LQRun/txt/list_user_mc.sh
@@ -8,4 +8,5 @@ declare -a example=("" "WW" "" "WZ")
 
 declare -a tmplist=('WpWp_qcd_madgraph' 'ZG_llG_MCatNLO' 'ZZ_llnunu_powheg' 'ZZ_llqq_MCatNLO' 'ZZ_llll_MCatNLO' 'ZZ_llll_powheg' 'ZZ_pythia8' 'ttHnobb_Powheg' 'ttHtobb_Powheg')
 
+
 declare -a hn=('DYJets_10to50'  'DYJets' 'WW' 'ZZ' 'WZ' 'TTJets_MG')
