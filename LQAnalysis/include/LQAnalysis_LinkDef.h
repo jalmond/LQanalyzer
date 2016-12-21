ad9deff4ece0d16037e7038ee28c539214f333cb
Sungbin Oh <sungbin.oh@cern.ch>
2016-12-21 18:18:11 +0900

    for pull request

--- LQAnalysis/include/LQAnalysis_LinkDef.h
+++ LQAnalysis/include/LQAnalysis_LinkDef.h
@@ -8,13 +8,6 @@
 
 #pragma link C++ class AnalyzerCore+;
 #pragma link C++ class ExampleAnalyzer+;
-#pragma link C++ class DiMuon_plots+;
-#pragma link C++ class HN_pair_di_muon+;
-#pragma link C++ class HN_pair_MM+;
-#pragma link C++ class Truth_HN_pair+;
-#pragma link C++ class ChargeFlip_e+;
-#pragma link C++ class Closure_cf+;
-#pragma link C++ class Scale_cf+;
 #pragma link C++ class HNDiElectron+;
 #pragma link C++ class FakeRateCalculator_El+;
 #pragma link C++ class SKTreeMaker+;
