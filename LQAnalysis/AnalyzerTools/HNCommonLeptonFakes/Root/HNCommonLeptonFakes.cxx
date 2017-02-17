31eeecd017e95fa2311fce930d5d22c47a6d5260
Jae Sung Kim <jae.sung.kim@cern.ch>
2017-02-17 12:10:09 +0900

    Trilep IDSF, PromptRate, Fake file changed

--- LQAnalysis/AnalyzerTools/HNCommonLeptonFakes/Root/HNCommonLeptonFakes.cxx
+++ LQAnalysis/AnalyzerTools/HNCommonLeptonFakes/Root/HNCommonLeptonFakes.cxx
@@ -53,8 +53,8 @@ void HNCommonLeptonFakes::InitialiseFake(){
 
   //==== Trilep
   //==== Using Large dXYSig muons
-  TFile* file_trilep_fake = TFile::Open( (lqdir + "/data/Fake/"+getenv("yeartag")+"/Trilep_Muon_FakeRate.root").c_str());
-  TFile* file_trilep_prompt = TFile::Open( (lqdir + "/data/Fake/"+getenv("yeartag")+"/Trilep_Muon_PromptRate.root").c_str());
+  TFile* file_trilep_fake = TFile::Open( (lqdir + "/data/Fake/"+getenv("yeartag")+"/Trilep_Muon_FakeRate_RunBCDEFGH_rereco.root").c_str());
+  TFile* file_trilep_prompt = TFile::Open( (lqdir + "/data/Fake/"+getenv("yeartag")+"/Trilep_Muon_PromptRate_RunBCDEF_rereco.root").c_str());
   CheckFile(file_trilep_fake);
   CheckFile(file_trilep_prompt);
 
