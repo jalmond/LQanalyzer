55431ae2566b343ce2ddd33e97349f022a1848c8
jalmond <jalmond@cern.ch>
2017-02-23 11:20:27 +0900

    jet smearing for met

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-4
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.8'
+export tag_numerator='.9'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-4.8
+    export CATTAG=v8-0-4.9
 fi
 
