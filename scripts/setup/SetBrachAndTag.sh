c96b849c284eb770544f51602dc9962867f1278c
jalmond <jalmond@cern.ch>
2017-02-23 12:23:45 +0900

    update Tracking SFs

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-4
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.9'
+export tag_numerator='.10'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-4.9
+    export CATTAG=v8-0-4.10
 fi
 
