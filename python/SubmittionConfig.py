d7159832a0a479b624d9a3d1563f51a9cfaabb00
jalmond <jalmond@cern.ch>
2017-03-15 03:05:16 +0900

    set debug to false

--- python/SubmittionConfig.py
+++ python/SubmittionConfig.py
@@ -1551,7 +1551,7 @@ sample_times=[]
 tmpnjobs_for_submittion=0
 
 ### rundebug=True will not submit any jobs and debug.txt file will be produced in ./
-rundebug=True
+rundebug=False
 if rundebug:
     file_debug = open("debug.txt","w")
     file_debug.write("DEBUG \n")
