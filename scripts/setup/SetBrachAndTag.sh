caa2bb7c55fb73c46aae26d49e6546105793f541
jalmond <jalmond@cern.ch>
2017-03-15 20:45:32 +0900

    add fatjet sel files

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-4
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.20'
+export tag_numerator='.21'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-4.20
+    export CATTAG=v8-0-4.21
 fi
 
