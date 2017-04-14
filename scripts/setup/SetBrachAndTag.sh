3de5a48a9c03a6add164daee4ba79e95fae4ef17
jalmond <jalmond@cern.ch>
2017-04-14 21:18:43 +0900

    new tag for sihyun

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-6
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.7'
+export tag_numerator='.8'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-6.7
+    export CATTAG=v8-0-6.8
 fi
 
