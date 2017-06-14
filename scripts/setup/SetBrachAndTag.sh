da667e6440ac6696282a83217ee1c1a466e8494f
jalmond <jalmond@cern.ch>
2017-06-14 20:58:43 +0900

    Merge HST from jaesung

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-7
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.13'
+export tag_numerator='.14'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-7.13
+    export CATTAG=v8-0-7.14
 fi
 
--- scripts/setup/git_newtag.sh
+++ scripts/setup/git_newtag.sh
@@ -4,7 +4,7 @@ if [[ $LQANALYZER_DIR == "" ]];
 fi
 
 ########## Tag index
-itag=".13"
+itag=".14"
 tagname=$CATVERSION$itag
 
 
