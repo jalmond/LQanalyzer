c104da3e4dd8127f4780b3b6277bd7601625041f
jalmond <jalmond@cern.ch>
2017-06-14 21:57:27 +0900

    update truth matching

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-7
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.14'
+export tag_numerator='.15'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-7.14
+    export CATTAG=v8-0-7.15
 fi
 
