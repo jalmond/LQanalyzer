3314373b320ae7e7d1a39f11b6d3a1d58a2e0b1f
jalmond <jalmond@cern.ch>
2017-07-07 19:46:36 +0900

    update MET sys

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v7-6-6
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.10'
+export tag_numerator='.11'
 if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
-    export CATTAG=v7-6-6.10
+    export CATTAG=v7-6-6.11
 fi
 
