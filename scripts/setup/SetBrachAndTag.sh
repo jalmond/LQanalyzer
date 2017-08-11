3e6cdcf05fc317697af47579dcd7915525d05e5c
jalmond <jalmond@cern.ch>
2017-08-11 22:04:30 +0900

    new tag with MET fix for JET shift

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v7-6-6
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.11'
+export tag_numerator='.12'
 if [[ '-d' == branch ]];
     then
     export CATTAG=
 else
-    export CATTAG=v7-6-6.11
+    export CATTAG=v7-6-6.12
 fi
 
