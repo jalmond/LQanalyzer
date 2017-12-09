33894e7d166c91ba704ccd286530c9be5254013a
jalmond <jalmond@cern.ch>
2017-12-09 20:19:35 +0900

    udpate for new signal lists

--- scripts/setup/SetBrachAndTag.sh
+++ scripts/setup/SetBrachAndTag.sh
@@ -1,10 +1,10 @@
 export CATVERSION=v8-0-7
 ### If there is a small bug/new code then new subtag is made
-export tag_numerator='.38'
+export tag_numerator='.39'
 if [[ $1 == 'branch' ]];
     then
     export CATTAG=
 else
-    export CATTAG=v8-0-7.38
+    export CATTAG=v8-0-7.39
 fi
 
--- scripts/setup/git_newtag.sh
+++ scripts/setup/git_newtag.sh
@@ -4,7 +4,7 @@ if [[ $LQANALYZER_DIR == "" ]];
 fi
 
 ########## Tag index
-itag=".38"
+itag=".39"
 tagname=$CATVERSION$itag
 
 
