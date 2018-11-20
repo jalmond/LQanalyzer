026fed12f9b0dc7f8351f18fcb0ae2ece8a8c1f7
jalmond <jalmond@cern.ch>
2018-11-20 14:14:42 +0900

    add submit file

---
+++ LQRun/HNSubmit/tets.sh
@@ -0,0 +1,3 @@
+sktree_80X  -a HN_pair_all -S DoubleEG -s SKTree_DiLepSkim -n 50 -F
+sktree_80X  -a HN_pair_all -S SingleMuon -s SKTree_DiLepSkim -n 50 -F
+sktree_80X  -a HN_pair_all -list hn_pair -s SKTree_DiLepSkim -n 50 -F
